ee443e24f39cf3b2123e480006d41061848ed130
amelia-agustin <nurindahameliaagustin@gmail.com>
2024-10-27 01:09:57 +0700

    First Commit

--- src/KamusSederhana.java
+++ src/KamusSederhana.java
@@ -1,5 +1,15 @@
+/**
+ * ini merupakan class interface yang menampung method translate (menerjemahkan kata yang diinputkan) dan inputword (untuk meminta inputan dari user)
+ */
 public interface KamusSederhana {
+    /**
+     * menerjemahkan kosa kata yang diinputkan oleh user
+     */
     void translate();
 
+    /**
+     * meminta inputan kosa kata dari user yang ingin ditranslate
+     * @return kata yang akan ditranslate
+     */
     String inputword();
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,9 +1,15 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * class ini digunakan untuk menyimpan kosa kata bahasa inggris beserta artinya, meminta inputan dari user mengenai kata apa yang ingin ditranslate, dan mentranslate kata yang diinputkan oleh user
+ */
 class KamusEngToIndo implements KamusSederhana {
     HashMap<String, String> englishToIndonesian = new HashMap<>(); //raname dari engToIndo ke englishToIndonesian
 
+    /**
+     * konstruktor default yang berisi kosa kata bahasa inggris beserta dengan artinya
+     */
     public KamusEngToIndo(){
         englishToIndonesian.put("apple","apel");
         englishToIndonesian.put("banana","pisang");
@@ -11,16 +17,25 @@ class KamusEngToIndo implements KamusSederhana {
         englishToIndonesian.put("dog","anjing");
         englishToIndonesian.put("house","rumah");
     }
+
+    /**
+     * method ini berfungsi untuk mentranslate kata dari bahasa inggris yang diinpukan oleh user ke bahasa indonesia
+     */
     @Override
     public void translate(){ //rename dari input word & extract interface
         //String word = inputword(); dilakukan inline variable setelah melakukan extract method
-        //String translate = englishToIndonesian.get(inputword()); //dilakukan inline variable
-        if (englishToIndonesian.get(inputword()) != null){ //hasil dari inline variable translate
-            System.out.println("Terjemahan dalam bahasa indonesia: "+ englishToIndonesian.get(inputword()));
+        String translate = englishToIndonesian.get(inputword());
+        if (translate != null){
+            System.out.println("Terjemahan dalam bahasa indonesia: "+ translate);
         }else {
             System.out.println("Kata tidak ditemukan!!!");
         }
     }
+
+    /**
+     * method ini berfungsi untuk meminta inputan berupa kosa kata yang ingin ditranslate oleh user dari bahasa inggris ke indonesia
+     * @return kata yang dimasukkan oleh pengguna untuk diterjemahkan.
+     */
     @Override
     public String inputword() { //extract method
         Scanner scanner = new Scanner(System.in);
@@ -29,8 +44,16 @@ class KamusEngToIndo implements KamusSederhana {
         return word;
     }
 }
+
+/**
+ * class ini digunakan untuk menyimpan kosa kata bahasa indonesia beserta terjemahan dalam bahasa inggrisnya, meminta inputan dari user mengenai kata apa yang ingin ditranslate ke bahasa inggris, dan mentranslate kata yang diinputkan oleh user
+ */
 class KamusIndoToEng implements KamusSederhana{
     HashMap<String, String > indonesianToEnglish = new HashMap<>(); //rename dari indoToEng
+
+    /**
+     * method ini merupakan method default yang hanya berisi daftar kosa kata bahasa indonesia beserta terjemahannya dalam bahasa inggris
+     */
     public KamusIndoToEng(){
         indonesianToEnglish.put("apel","apple");
         indonesianToEnglish.put("pisang","banana");
@@ -38,17 +61,25 @@ class KamusIndoToEng implements KamusSederhana{
         indonesianToEnglish.put("anjing","dog");
         indonesianToEnglish.put("rumah","house");
     }
+
+    /**
+     * method ini berfungsi untuk mentranslate kosa kata bahasa indonesia ke bahasa inggris
+     */
     @Override
     public void translate(){ //rename dari inputword
-        String word = inputword();
-        String translate = indonesianToEnglish.get(word); //bisa dilakukan inline variable
+        String translate = indonesianToEnglish.get(inputword()); //bisa dilakukan inline variable
         if (translate != null){
             System.out.println("Terjemahan dalam bahasa indonesia: "+translate);
         }else {
             System.out.println("Kata tidak ditemukan!!!");
         }
     }
-@Override
+
+    /**
+     * method ini digunakan untuk meminta inputan kosa kata bahasa indonesia dari user yang akan ditranslate ke bahasa inggruis
+     * @return word mengembalikan word yang merupakan variabel untuk menyimpan inputan user mengenai kosa kata yang ingin ditranslate
+     */
+    @Override
     public String inputword() {
         Scanner scanner = new Scanner(System.in);
         String word;
@@ -57,7 +88,17 @@ class KamusIndoToEng implements KamusSederhana{
         return word;
     }
 }
+
+/**
+ * class ini merupakan class utama yang menjalankan semua kode, dalam kelas ini membuat objek dari kelas KamusEngToIndo dan KamusIndoToEng,
+ * menampilkan menu utama dari aplikasi yaitu translate kata ke bahasa inggris dan indonesia,
+ * meminta inputan user untuk memilih menu yang diinginkan lalu membuat kondisi sebagai respon dari inputan user dan memanggil method method dari class nya masing-masing untuk melakukan tugasnya sesuai dengan menu yang diinputkan oleh user
+ */
 public class MainApp{ //hasil dari move method
+    /**
+     * method utama yang menjalankan program
+     * @param args Argumen dari command line (tidak digunakan)
+     */
     public static void main(String[] args) {
         KamusEngToIndo englishtoindo = new KamusEngToIndo();
         KamusIndoToEng indotoenglish = new KamusIndoToEng();
